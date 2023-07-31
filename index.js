a2570d8870a3ac14641de0374c49d693b7d51af5
Sammed100 <sammedjain100@gmail.com>
2023-07-31 17:47:08 +0530

    Change

--- index.js
+++ index.js
@@ -11,20 +11,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json())
 
 const PORT = process.env.PORT || 3000;
-// function getCurrentDateTime() {
-//   const date = new Date();
-  
-//   const year = date.getFullYear();
-//   const month = String(date.getMonth() + 1).padStart(2, '0');
-//   const day = String(date.getDate()).padStart(2, '0');
-  
-//   const hours = String(date.getHours()).padStart(2, '0');
-//   const minutes = String(date.getMinutes()).padStart(2, '0');
-//   const seconds = String(date.getSeconds()).padStart(2, '0');
-//   const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
-  
-//   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-// }
+
 app.get('/api',(req,res)=>{
   const currentTimestamp1 = moment().tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss.SSS');
   console.log(`Current date and time: ${currentTimestamp1}`);
@@ -40,6 +27,7 @@ connection.query(`SELECT * FROM data_table WHERE language='${language}' AND TIME
    console.log(`Current date and time: ${currentTimestamp2}`);
   }
 });
+
     
 })
 app.post('/api',(req,res)=>{
